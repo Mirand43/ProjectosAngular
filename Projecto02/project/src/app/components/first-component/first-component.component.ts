ae1f65b144c0fedbb17cb52a6f29051c42caf06d
Mirand43 <antoniomq99@gmail.com>
2023-08-17 13:13:55 +0100

    upload project

---
+++ Projecto02/project/src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.css']
+})
+export class FirstComponentComponent implements OnInit{
+  name: string = 'António Quintas';
+  age: number = 24;
+  job: string = 'Programador';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+}
